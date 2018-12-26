f86ba373b50f8f89c12a8f9c3df50fcbdcbb42c4
Håvard Falk <havard.falk@bekk.no>
2018-12-26 15:57:39 +0100

    Add appcenter

--- android/app/src/main/java/com/wallas/MainApplication.java
+++ android/app/src/main/java/com/wallas/MainApplication.java
@@ -3,6 +3,9 @@ package com.wallas;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
+import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
+import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.henninghall.date_picker.DatePickerPackage;
@@ -27,6 +30,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
+            new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
+            new AppCenterReactNativePackage(MainApplication.this),
             new ReactNativeConfigPackage(),
             new VectorIconsPackage(),
             new DatePickerPackage(),
