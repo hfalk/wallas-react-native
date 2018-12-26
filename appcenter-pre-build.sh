6a96545620813b07a1667559ce5aceb5cdf8bde1
Sigurd Hagen Falk <sigurd.falk@gmail.com>
2018-12-26 16:06:15 +0100

    replace APPCENTER_SECRET with secret from env-var

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -4,3 +4,7 @@
   echo "API_USERNAME='$API_USERNAME'"
   echo "API_TOKEN='$API_TOKEN'"
 } >.env
+
+echo '{"app_secret": "'$APP_CENTER_SECRET'"}' >android/app/src/main/assets/appcenter-config.json
+
+plutil -replace AppSecret -string $APP_CENTER_SECRET ios/wallas/AppCenter-Config.plist
