e24e68a5e5a76a69ca8b84d3818790011d10f223
Sigurd Hagen Falk <sigurd.falk@gmail.com>
2018-12-26 16:10:51 +0100

    Update appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -5,9 +5,9 @@
   echo "API_TOKEN='$API_TOKEN'"
 } >.env
 
-echo '{"app_secret": "'$APP_CENTER_SECRET'"}' >android/app/src/main/assets/appcenter-config.json
+echo '{"app_secret": "'$APP_CENTER_SECRET_ANDROID'"}' >android/app/src/main/assets/appcenter-config.json
 
-plutil -replace AppSecret -string $APP_CENTER_SECRET ios/wallas/AppCenter-Config.plist
+plutil -replace AppSecret -string $APP_CENTER_SECRET_IOS ios/wallas/AppCenter-Config.plist
 
 cat android/app/src/main/assets/appcenter-config.json
 cat os/wallas/AppCenter-Config.plist
