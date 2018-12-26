5e87f0d0fbcaa92a0875107a74f4b8cdc9374b2b
Sigurd Hagen Falk <sigurd.falk@gmail.com>
2018-12-26 14:43:00 +0100

    Update appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-pwd
-ls
-echo 'API_USERNAME='$API_USERNAME''
-echo 'API_TOKEN='$API_TOKEN''
-
 {
   echo 'API_USERNAME='$API_USERNAME''
   echo 'API_TOKEN='$API_TOKEN''
 } >.env
+
+pwd
+ls -a
+echo 'API_USERNAME='$API_USERNAME''
+echo 'API_TOKEN='$API_TOKEN''
